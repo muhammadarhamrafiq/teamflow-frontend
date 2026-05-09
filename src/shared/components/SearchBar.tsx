b81400f2f01fb7373684a1e29d5185e01e593293
muhammadarhamrafiq <arhmraf@gmail.com>
2026-05-09 16:28:17 +0500

    chore: added debouncing in search

--- src/shared/components/SearchBar.tsx
+++ src/shared/components/SearchBar.tsx
@@ -1,12 +1,40 @@
 import type { InputFieldProps } from "@/app";
 import { Search } from "@hugeicons/core-free-icons";
 import { HugeiconsIcon } from "@hugeicons/react";
+import { useEffect, useState, type ChangeEvent } from "react";
 import { InputGroup, InputGroupAddon, InputGroupInput } from "./ui/input-group";
 
 const SearchBar = ({ value, onChange, className }: InputFieldProps) => {
+  const [localValue, setLocalValue] = useState(value ?? "");
+
+  useEffect(() => {
+    setLocalValue(value ?? "");
+  }, [value]);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (localValue === value) return;
+
+      onChange({
+        target: { value: localValue } as EventTarget & HTMLInputElement,
+        currentTarget: { value: localValue } as EventTarget & HTMLInputElement,
+      } as ChangeEvent<HTMLInputElement>);
+    }, 300);
+
+    return () => clearTimeout(timer);
+  }, [localValue, onChange, value]);
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setLocalValue(e.target.value);
+  };
+
   return (
     <InputGroup className={className}>
-      <InputGroupInput placeholder="search" value={value} onChange={onChange} />
+      <InputGroupInput
+        placeholder="search"
+        value={localValue}
+        onChange={handleChange}
+      />
       <InputGroupAddon>
         <HugeiconsIcon icon={Search} />
       </InputGroupAddon>
